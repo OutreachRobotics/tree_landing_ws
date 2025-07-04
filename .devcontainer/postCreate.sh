c3ba16b51f456e398f63840866d9d8a893e150d3
chcaya <cayc2401@usherbrooke.ca>
2025-07-04 15:20:54 +0000

    Fixed docker

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,5 +4,5 @@ rosdep update
 rosdep install --from-paths /home/$(whoami)/tree_landing_ws/src --ignore-src -r -y
 
 cd /home/$(whoami)/tree_landing_ws/
-bash -c "source /opt/ros/humble/setup.bash && MAKEFLAGS='-j1' colcon build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --symlink-install" # --packages-up-to tree_landing"
+bash -c "source /opt/ros/humble/setup.bash && MAKEFLAGS='-j1' colcon build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --packages-skip ardupilot_dds_tests --symlink-install" # --packages-up-to tree_landing"
 bash -c "source /home/$(whoami)/tree_landing_ws/install/local_setup.bash"
